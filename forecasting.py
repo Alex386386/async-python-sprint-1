cbf6c448e6f400d85a9ccf84617cbbbc2a503e47
Alex386386 <mamonov40600@mail.ru>
2023-07-06 11:45:59 +0300

    commit #0.2

--- forecasting.py
+++ forecasting.py
@@ -1,40 +1,41 @@
-import logging
 import shutil
+from multiprocessing import Manager
 from multiprocessing.pool import Pool
 
 from tasks import (DataFetchingTask, DataCalculationTask, DataAggregationTask,
                    DataAnalyzingTask, )
-from utils import BASE_DIR
-from utils import CITIES
-
-logger = logging.getLogger('mylog')
-logging.basicConfig(level=logging.INFO)
+from utils import BASE_DIR, CITIES
 
 
 def start_aggregation():
-    dataaggregationtask = DataAggregationTask()
+    data_aggregation_task = DataAggregationTask(BASE_DIR / 'results')
 
-    subresult, result = dataaggregationtask.collect_and_overwrite()
-    new_result = dataaggregationtask.filter_of_data(subresult, result)
-    dataaggregationtask.write_filtered_data(new_result)
+    sub_result, result = data_aggregation_task.collect_and_overwrite()
+    new_result = data_aggregation_task.filtering_of_data(sub_result, result)
+    data_aggregation_task.write_filtered_data(new_result)
 
 
 def forecast_weather():
     """
     Анализ погодных условий по городам
     """
-    datafetchingtask = DataFetchingTask()
-    datacalculationtask = DataCalculationTask()
-    dataanalyzingtask = DataAnalyzingTask()
+    manager = Manager()
+    data_queue = manager.Queue()
+    data_fetching_task = DataFetchingTask(BASE_DIR / 'output_data', data_queue)
+    data_calculation_task = DataCalculationTask(BASE_DIR / 'output_data',
+                                                BASE_DIR / 'analyzed_data',
+                                                data_queue)
+    data_analyzing_task = DataAnalyzingTask(BASE_DIR / 'results')
 
-    with Pool() as pool:
-        pool.map(datafetchingtask.get_data, CITIES.keys())
+    with Pool() as fetch_pool:
+        fetch_pool.map(data_fetching_task.get_data, CITIES.keys())
+    data_queue.put(None)
 
-    datacalculationtask.data_processing()
+    data_calculation_task.process_queue()
 
     start_aggregation()
 
-    dataanalyzingtask.get_the_answer()
+    data_analyzing_task.get_the_answer()
 
     shutil.rmtree(BASE_DIR / 'output_data')
     shutil.rmtree(BASE_DIR / 'analyzed_data')
--- tasks.py
+++ tasks.py
@@ -3,114 +3,122 @@ import csv
 import json
 import logging
 from concurrent.futures import ThreadPoolExecutor
+from multiprocessing import Queue
 from pathlib import Path
-from typing import List, Dict
 
 from external.analyzer import load_data, analyze_json, dump_data
 from external.client import YandexWeatherAPI
 from utils import get_url_by_city_name, CITIES_NAMES, BASE_DIR
 
+logger = logging.getLogger('mylog')
+logging.basicConfig(level=logging.INFO)
+
 
 class DataFetchingTask:
-    def __init__(self):
-        self.logger = logging.getLogger('mylog')
+    def __init__(self, downloads_dir: Path, queue: Queue):
+        self.downloads_dir = downloads_dir
+        self.queue = queue
 
-    def download_data(self, data: Dict, city: str):
-        filename = f'{city}.json'
-        downloads_dir = BASE_DIR / 'output_data'
-        downloads_dir.mkdir(exist_ok=True)
-        archive_path = downloads_dir / filename
+    def download_data(self, data: dict, city: str) -> None:
         if data:
+            filename = f'{city}.json'
+            self.downloads_dir.mkdir(exist_ok=True)
+            archive_path = self.downloads_dir / filename
             with open(archive_path, 'w') as file:
                 formatted_data = json.dumps(data, indent=2)
                 file.write(formatted_data)
-            self.logger.info(
+            logger.info(
                 f'Информация о городе {CITIES_NAMES[city]} загружена.')
 
-    def get_data(self, city_name: str):
+    def get_data(self, city_name: str) -> None:
         try:
             url_with_data = get_url_by_city_name(city_name)
             resp = YandexWeatherAPI.get_forecasting(url_with_data)
             if resp:
                 self.download_data(resp, city_name)
+                self.queue.put(city_name)
         except Exception as ex:
-            self.logger.error(f'Возникли проблемы с получением данных: {ex}.')
+            logger.error(f'Возникли проблемы с получением данных: {ex}.')
 
 
 class DataCalculationTask:
-    def __init__(self):
-        self.logger = logging.getLogger('mylog')
+    def __init__(self, data_dir: Path, write_dir: Path, queue: Queue):
+        self.data_dir = data_dir
+        self.write_dir = write_dir
+        self.queue = queue
 
-    def process_file(self, file_path: Path):
+    def process_file(self, file_path: Path) -> None:
         file_path = str(file_path).split('/')[-1]
-        data_dir = BASE_DIR / 'output_data'
-        write_dir = BASE_DIR / 'analyzed_data'
-        write_dir.mkdir(exist_ok=True)
-        output_path = str(write_dir / file_path)
-        data = load_data(f'{data_dir / file_path}')
+        self.write_dir.mkdir(exist_ok=True)
+        output_path = str(self.write_dir / file_path)
+        data = load_data(f'{self.data_dir / file_path}')
         data = analyze_json(data)
         dump_data(data, output_path)
-        self.logger.info(
-            f'Информация из файла {file_path} обработана успешно.')
+        logger.info(f'Информация из файла {file_path} обработана успешно.')
 
-    def data_processing(self):
-        data_dir = BASE_DIR / 'output_data'
-        directory = Path(data_dir)
-
-        with ThreadPoolExecutor() as executor:
-            executor.map(self.process_file, directory.glob('*.json'))
-        self.logger.info('Все файлы прошли анализ.')
+    def process_queue(self) -> None:
+        while True:
+            file_name = self.queue.get()
+            if file_name is None:
+                break
+            file_path = Path(f'{file_name}.json')
+            self.process_file(file_path)
 
 
 class DataAggregationTask:
-    def __init__(self):
-        self.logger = logging.getLogger('mylog')
+    def __init__(self, results_dir: Path):
+        self.results_dir = results_dir
 
-    def process_file(self, file_path: Path):
+    @staticmethod
+    def process_file(file_path: Path) -> (dict[str, dict[str, float]],
+                                          list[list[str]]):
         with open(file_path, 'r') as file:
             data = json.load(file)
-            filter_days = [x for x in data['days'] if
-                           x['hours_start'] == 9 and x['hours_end'] == 19]
-            city_name = file_path.stem
-            filtered_data = {CITIES_NAMES[city_name]: filter_days}
-            city_temp_data = [CITIES_NAMES[city_name], 'Температура, среднее']
-            city_hours_data = ['', 'Без осадков, часов']
+        filter_days = [x for x in data['days'] if
+                       x['hours_start'] == 9 and x['hours_end'] == 19]
+        city_name = file_path.stem
+        filtered_data = {CITIES_NAMES[city_name]: filter_days}
+        city_temp_data = [CITIES_NAMES[city_name], 'Температура, среднее']
+        city_hours_data = ['', 'Без осадков, часов']
 
-            for day in filtered_data[CITIES_NAMES[city_name]]:
-                city_temp_data.append(day['temp_avg'])
-                city_hours_data.append(day['relevant_cond_hours'])
+        for day in filtered_data[CITIES_NAMES[city_name]]:
+            city_temp_data.append(day['temp_avg'])
+            city_hours_data.append(day['relevant_cond_hours'])
 
-            average_temp = sum(city_temp_data[-3:]) / 3
-            average_hours = sum(city_hours_data[-3:]) / 3
-            city_temp_data.append(average_temp)
-            city_hours_data.append(average_hours)
-            city_hours_data.append('')
-            subresult = {CITIES_NAMES[city_name]: {'temp': average_temp,
-                                                   'hours': average_hours}}
-            result = [city_temp_data, city_hours_data]
-            self.logger.info(
-                f'По файлу {file_path} произведён подсчёт значений.')
-            return subresult, result
+        # Подсчёт среднего значения температуры для города.
+        average_temp = sum(city_temp_data[2:]) / len(city_temp_data[2:])
+        # Подсчёт среднего значения количества часов без осадков.
+        average_hours = sum(city_hours_data[2:]) / len(city_hours_data[2:])
+        city_temp_data.append(average_temp)
+        city_hours_data.append(average_hours)
+        city_hours_data.append('')
+        sub_result = {CITIES_NAMES[city_name]: {'temp': average_temp,
+                                                'hours': average_hours}}
+        result = [city_temp_data, city_hours_data]
+        logger.info(f'По файлу {file_path} произведён подсчёт значений.')
+        return sub_result, result
 
-    def collect_and_overwrite(self):
+    def collect_and_overwrite(self) -> (dict[str, dict[str, float]],
+                                        list[list[str]]):
         directory = BASE_DIR / 'analyzed_data'
         result = [('Город', 'Ед.изм', '2022-05-26', '2022-05-27',
                    '2022-05-28', 'Среднее', 'Рейтинг')]
-        subresult = {}
+        sub_result = {}
 
         with ThreadPoolExecutor() as executor:
             futures = [executor.submit(self.process_file, file_path)
                        for file_path in directory.glob('*.json')]
             for future in concurrent.futures.as_completed(futures):
                 sr, res = future.result()
-                subresult.update(sr)
+                sub_result.update(sr)
                 result.extend(res)
-        self.logger.info('Подсчёт средней температуры произведён.')
-        return subresult, result
+        logger.info('Подсчёт средней температуры произведён.')
+        return sub_result, result
 
-    def filter_of_data(self, subresult: Dict[str, Dict[str, float]],
-                       result: List[List[str]]):
-        sorted_data = sorted(subresult.items(),
+    @staticmethod
+    def filtering_of_data(sub_result: dict[str, dict[str, float]],
+                          result: list[list[str]]) -> list[list[str]]:
+        sorted_data = sorted(sub_result.items(),
                              key=lambda x: (x[1]['temp'], x[1]['hours']),
                              reverse=True)
         counter: int = 1
@@ -119,40 +127,38 @@ class DataAggregationTask:
                 if city[0] in data:
                     data.append(str(counter))
                     counter += 1
-        self.logger.info('Добавлен рейтинг для городов.')
+        logger.info('Добавлен рейтинг для городов.')
         return result
 
-    def write_filtered_data(self, result: List[List[str]]):
-        results_dir = BASE_DIR / 'results'
-        results_dir.mkdir(exist_ok=True)
+    def write_filtered_data(self, result: list[list[str]]) -> None:
+        self.results_dir.mkdir(exist_ok=True)
         file_name = 'result.csv'
-        file_path = results_dir / file_name
+        file_path = self.results_dir / file_name
         with open(file_path, 'w', encoding='utf-8') as f:
             writer = csv.writer(f, dialect='unix')
             writer.writerows(result)
-        self.logger.info(f'Данные записаны по адресу {file_path}.')
+        logger.info(f'Данные записаны по адресу {file_path}.')
 
 
 class DataAnalyzingTask:
-    def __init__(self):
-        self.logger = logging.getLogger('mylog')
+    def __init__(self, directory: Path):
+        self.directory = directory
 
-    def get_the_answer(self):
-        directory = BASE_DIR / 'results'
-        file_path = directory / 'result.csv'
+    def get_the_answer(self) -> None:
+        file_path = self.directory / 'result.csv'
         result = {}
         with open(file_path, 'r') as file:
             reader = csv.reader(file)
             reader = list(reader)
-            for idx, row in enumerate(reader):
-                if row[-1] != '' and row[-1] != 'Рейтинг':
-                    result[row[0]] = (row[-2], reader[idx + 1][-2], row[-1])
-            result = sorted(result.items(), key=lambda item: int(item[1][-1]))
-            self.logger.info('Вывод результатов анализа.')
-            for idx, city in enumerate(result):
-                if idx == 0:
-                    print(city[0])
-                elif idx > 0 and city[1][0] == result[idx - 1][1][0]:
-                    print(city[0])
-                elif idx > 0 and city[1][0] != result[idx - 1][1][0]:
-                    break
+        for idx, row in enumerate(reader):
+            if row[-1] != '' and row[-1] != 'Рейтинг':
+                result[row[0]] = (row[-2], reader[idx + 1][-2], row[-1])
+        result = sorted(result.items(), key=lambda item: int(item[1][-1]))
+        logger.info('Вывод результатов анализа.')
+        for idx, city in enumerate(result):
+            if idx == 0:
+                print(city[0])
+            elif idx > 0 and city[1][0] == result[idx - 1][1][0]:
+                print(city[0])
+            elif idx > 0 and city[1][0] != result[idx - 1][1][0]:
+                break
